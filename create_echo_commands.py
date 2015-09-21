e1b1393c37a08d715f0e65cc41054a6f547bd3a1
jtmacdonald <jtmacdonald@gmail.com>
2015-09-21 17:37:11 +0100

    working on instructions

--- create_echo_commands.py
+++ create_echo_commands.py
@@ -9,10 +9,11 @@ def get_well_ID(well_number):
 	return row_letter + str(col)
 
 class InstrClass(object):
-        def __init__(self, source_well, dest_well, volume, source_instantiated):
+        def __init__(self, source_well, dest_well, volume, chemical, source_instantiated):
  		self.source_well = source_well
 		self.dest_well = dest_well
 		self.volume = volume
+		self.chemical = chemical
 		self.source_instantiated = source_instantiated
 		return
 
@@ -31,7 +32,7 @@ conc_lists = [
 ]
 
 # all volumes in uL
-dest_final_volume = 10
+dest_final_volume = 10.0
 
 
 instruction_list = []
@@ -40,15 +41,26 @@ for condition in product(*conc_lists):
     dest_well = get_well_ID(count)
     print dest_well + "\t" +str(condition)
 
-    # loop over stocks to create instructions
-    for idx, val in enumerate(condition):
+    total_well_vol = 0
+    # loop over stocks to create instructions:
+    for idx, conc in enumerate(condition):
         stock_tup = stocks.items()[idx]
 	stock_chem = stock_tup[0]
 	stock_conc = stock_tup[1]
+	
+	if conc > 0:
+	    volume_to_transfer = (float(conc)/float(stock_conc)) * float(dest_final_volume)
+	    instr = InstrClass("", dest_well, volume_to_transfer, stock_chem, False)
+	    print "transfer: " + str(volume_to_transfer) + " from " + stock_chem + ":" + str(stock_conc) + " stock to destination well: " + dest_well + " for final conc " + str(conc)
+            total_well_vol += volume_to_transfer
+            #print stock_tup
+	    instruction_list.append(instr)
 
-	instr = InstrClass("", dest_well, 0, False)
-        print stock_tup
-	instruction_list.append(instr)
+    # finally make up volume with water:
+    volume_to_transfer = dest_final_volume - total_well_vol
+    instr = InstrClass("", dest_well, volume_to_transfer, "water", False)
+    instruction_list.append(instr)
+    print "transfer: " + str(volume_to_transfer) + " from water stock to destination well: " + dest_well + " for final volume " + str(dest_final_volume)
         	
     count += 1
 
