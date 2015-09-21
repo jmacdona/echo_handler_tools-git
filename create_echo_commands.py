1dc0da025596b7e7efb2a7bd452be14fa8a547ce
jtmacdonald <jtmacdonald@gmail.com>
2015-09-21 17:03:14 +0100

    working on script for echo handler

--- create_echo_commands.py
+++ create_echo_commands.py
@@ -1,6 +1,14 @@
 from itertools import product
 
 
+def get_well_ID(well_number):
+	letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','','','','','','','','','','','']
+	row = well_number // 24
+	col = (well_number % 24) + 1
+    	row_letter = letters[row] 
+	return row_letter + str(col)
+
+
 stocks = {
     'ATP': 100, 
     'NAD': 100, 
@@ -15,8 +23,12 @@ conc_lists = [
 
 
 
+count = 0
 for items in product(*conc_lists):
-    print items
+    dest_well = get_well_ID(count)
+    print dest_well + "\t" +str(items)
+	
+    count += 1
 
 
 
